cd595cda737539b1372bd3e1898a15e2815720ee
N'CHE <nche.song@gmail.com>
2022-03-13 22:33:40 +0900

    Create project

---
+++ src/main/java/io/nche/restapidocs/RestApiDocsMultiJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.nche.restapidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDocsMultiJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiDocsMultiJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nche/restapidocs/RestApiDocsMultiJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nche.restapidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDocsMultiJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
