e5f4a6142782b18c092111e88bc9b86ed82057db
N'CHE <nche.song@gmail.com>
2022-03-14 20:23:46 +0900

    Add module-web
    - DB와 접근하고 로직을 처리하는 api service와 client 요청/응답을 처리하는 web controller를 분리하기 위해 모듈을 추가

---
+++ module-web/src/main/java/io/nche/web/ModuleWebApplication.java
@@ -0,0 +1,11 @@
+package io.nche.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleWebApplication.class, args);
+    }
+}
---
+++ module-web/src/test/java/io/nche/web/ModuleWebApplicationTests.java
@@ -0,0 +1,11 @@
+package io.nche.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ModuleWebApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
